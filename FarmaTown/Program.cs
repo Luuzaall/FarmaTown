488bee4caa08e25e0448524143a0d96f855d41ca
valen <valenemartini@gmail.com>
2021-09-11 13:15:57 -0300

    Test de db

--- FarmaTown/Program.cs
+++ FarmaTown/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FarmaTown.Datos;
 
 namespace FarmaTown
 {
@@ -14,6 +16,15 @@ namespace FarmaTown
         [STAThread]
         static void Main()
         {
+            DBHelper db = new DBHelper();
+            DataTable tabla = db.consultarTabla("TiposMedicamentos");
+            foreach (DataRow dataRow in tabla.Rows)
+            {
+                foreach (var item in dataRow.ItemArray)
+                {
+                    Console.WriteLine(item);
+                }
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
