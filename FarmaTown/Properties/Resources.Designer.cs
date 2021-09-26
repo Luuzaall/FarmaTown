36dc7a1779eb98678cdc9a5541d0878775d09a0a
romerossi <romeliarossi@gmail.com>
2021-09-26 18:06:33 -0300

    barrios

--- FarmaTown/Properties/Resources.Designer.cs
+++ FarmaTown/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace FarmaTown.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap barrios {
+            get {
+                object obj = ResourceManager.GetObject("barrios", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap coworkers {
             get {
                 object obj = ResourceManager.GetObject("coworkers", resourceCulture);
