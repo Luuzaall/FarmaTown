3ddec9645087c5ba69a63012eff4beabb750ff63
romerossi <romeliarossi@gmail.com>
2021-09-26 19:07:10 -0300

    clase localidad

--- FarmaTown/Logica/Localidad.cs
+++ FarmaTown/Logica/Localidad.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FarmaTown.Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,60 @@ namespace FarmaTown.Logica
         public string Nombre { get; set; }
         public bool Borrado { get; set; }
 
-        LocalidadDao oLocalidadDao();
+        LocalidadDao oLocalidadDao;
+
+        public Localidad()
+        {
+            oLocalidadDao = new LocalidadDao();
+        }
+
+        public List<Localidad> recuperarCParam(string nombre)
+        {
+            return this.oLocalidadDao.recuperarCParam(nombre);
+        }
+
+        public List<Localidad> recuperarTodos(bool esCBorrados)
+        {
+            return this.oLocalidadDao.recuperarTodos(esCBorrados);
+        }
+
+        public Localidad traerLocalidad(int idLocalidad)
+        {
+            return this.oLocalidadDao.traer(idLocalidad);
+        }
+
+        public Localidad traerLocalidad(string nombre)
+        {
+            return this.oLocalidadDao.traer(nombre);
+        }
+
+        public bool crearLocalidad(Localidad nuevaLocalidad)
+        {
+            return validar(this.oLocalidadDao.crear(nuevaLocalidad));
+
+        }
+
+        public bool actualizar(Localidad oLocalidad)
+        {
+            return validar(this.oLocalidadDao.actualizar(oLocalidad));
+
+        }
+
+        public bool cambiarEstado(Localidad oLocalidad, bool seHabilita)
+        {
+            return validar(this.oLocalidadDao.cambiarEstado(oLocalidad, false));
+
+        }
+
+        private bool validar(int resultado)
+        {
+            if (resultado == 0)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
 
     }
 }
