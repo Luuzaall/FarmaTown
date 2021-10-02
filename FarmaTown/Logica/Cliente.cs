873c649ee33981ecbbfa009e8b707dd029a063dd
valen <83437771+valencurler@users.noreply.github.com>
2021-10-02 16:42:44 -0300

    inicio de clientes y ventas parte de datos

--- FarmaTown/Logica/Cliente.cs
+++ FarmaTown/Logica/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FarmaTown.Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,31 @@ namespace FarmaTown.Logica
 {
     public class Cliente
     {
-        public int IdCliente {get; set;}
+        public int IdCliente { get; set; }
         public int NroDoc { get; set; }
         public TipoDocumento TipoDoc { get; set; }
         public string Apellido { get; set; }
         public string Nombre { get; set; }
         public string Calle { get; set; }
         public int Numero { get; set; }
-        public Barrio IdBarrio { get; set; }
+        public Barrio Barrio { get; set; }
         public bool Borrado { get; set; }
 
+        ClienteDao oCliente;
+
+        public Cliente()
+        {
+            oCliente = new ClienteDao();
+        }
+
+        public List<Cliente> recuperarTodos(bool esCBorrados)
+        {
+            return this.oCliente.recuperarTodos(esCBorrados);
+        }
+
+        public List<Cliente> recuperarConParam(string nom, string apellido, string nroDoc, int idTipoDoc, string calle, int numCalle, int idCliente)
+        {
+            return oCliente.recuperarConParam(nom, apellido, nroDoc, idTipoDoc, calle, numCalle, idCliente);
+        }
     }
 }
--- FarmaTown/Logica/Venta.cs
+++ FarmaTown/Logica/Venta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FarmaTown.Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +10,33 @@ namespace FarmaTown.Logica
     public class Venta
     {
         public int IdVenta { get; set; }
-        public Farmacia IdFarmacia { get; set; }
+        public Farmacia Farmacia { get; set; }
         public int NroFactura { get; set; }
-        public Cliente IdCliente { get; set; }
+        public int TipoFactura { get; set; }
+        public Cliente Cliente { get; set; }
         public int FechaFactura { get; set; }
-        public Empleado IdEmpleado { get; set; }
+        public Empleado Empleado { get; set; }
         public bool Borrado { get; set; }
 
+        VentaDao oVenta;
+        public Venta()
+        {
+            oVenta = new VentaDao();
+        }
+
+        public bool crearVenta(Venta nuevaVenta)
+        {
+            return validar(this.oVenta.crear(nuevaVenta));
+        }
+
+        private bool validar(int resultado)
+        {
+            if (resultado == 0)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
     }
 }
