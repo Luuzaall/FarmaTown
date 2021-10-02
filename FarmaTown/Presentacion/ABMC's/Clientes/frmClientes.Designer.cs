c84c605f97e1ce34c9f46d82490631ae916c8a60
romerossi <romeliarossi@gmail.com>
2021-10-02 18:46:56 -0300

    poco avance frm

--- FarmaTown/Presentacion/ABMC's/Clientes/frmClientes.Designer.cs
+++ FarmaTown/Presentacion/ABMC's/Clientes/frmClientes.Designer.cs
@@ -30,14 +30,280 @@ namespace FarmaTown.Presentacion.ABMC_s.Clientes
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClientes));
+            this.gbFiltros = new System.Windows.Forms.GroupBox();
+            this.lblBarrio = new System.Windows.Forms.Label();
+            this.txtbNumCalle = new System.Windows.Forms.TextBox();
+            this.lblNumCalle = new System.Windows.Forms.Label();
+            this.txtbCalle = new System.Windows.Forms.TextBox();
+            this.lblCalle = new System.Windows.Forms.Label();
+            this.txtbApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtbNombre = new System.Windows.Forms.TextBox();
+            this.txtbNumDoc = new System.Windows.Forms.TextBox();
+            this.txtbPasaporteNro = new System.Windows.Forms.TextBox();
+            this.lblAvisoNroDoc = new System.Windows.Forms.Label();
+            this.lblNumDoc = new System.Windows.Forms.Label();
+            this.btnLimpiar = new CustomControls.RJControls.RJButton();
+            this.lblNomCli = new System.Windows.Forms.Label();
+            this.btnConsultar = new CustomControls.RJControls.RJButton();
+            this.cboTipoDoc = new System.Windows.Forms.ComboBox();
+            this.lblTipoDoc = new System.Windows.Forms.Label();
+            this.cboBarrio = new System.Windows.Forms.ComboBox();
+            this.gbFiltros.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gbFiltros
+            // 
+            this.gbFiltros.BackColor = System.Drawing.Color.Transparent;
+            this.gbFiltros.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.gbFiltros.Controls.Add(this.cboBarrio);
+            this.gbFiltros.Controls.Add(this.lblBarrio);
+            this.gbFiltros.Controls.Add(this.txtbNumCalle);
+            this.gbFiltros.Controls.Add(this.lblNumCalle);
+            this.gbFiltros.Controls.Add(this.txtbCalle);
+            this.gbFiltros.Controls.Add(this.lblCalle);
+            this.gbFiltros.Controls.Add(this.txtbApellido);
+            this.gbFiltros.Controls.Add(this.lblApellido);
+            this.gbFiltros.Controls.Add(this.txtbNombre);
+            this.gbFiltros.Controls.Add(this.txtbNumDoc);
+            this.gbFiltros.Controls.Add(this.txtbPasaporteNro);
+            this.gbFiltros.Controls.Add(this.lblAvisoNroDoc);
+            this.gbFiltros.Controls.Add(this.lblNumDoc);
+            this.gbFiltros.Controls.Add(this.btnLimpiar);
+            this.gbFiltros.Controls.Add(this.lblNomCli);
+            this.gbFiltros.Controls.Add(this.btnConsultar);
+            this.gbFiltros.Controls.Add(this.cboTipoDoc);
+            this.gbFiltros.Controls.Add(this.lblTipoDoc);
+            this.gbFiltros.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold);
+            this.gbFiltros.Location = new System.Drawing.Point(25, 30);
+            this.gbFiltros.Name = "gbFiltros";
+            this.gbFiltros.Size = new System.Drawing.Size(877, 266);
+            this.gbFiltros.TabIndex = 23;
+            this.gbFiltros.TabStop = false;
+            this.gbFiltros.Text = "Filtros";
+            // 
+            // lblBarrio
+            // 
+            this.lblBarrio.AutoSize = true;
+            this.lblBarrio.BackColor = System.Drawing.Color.Transparent;
+            this.lblBarrio.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBarrio.Location = new System.Drawing.Point(497, 118);
+            this.lblBarrio.Name = "lblBarrio";
+            this.lblBarrio.Size = new System.Drawing.Size(71, 23);
+            this.lblBarrio.TabIndex = 37;
+            this.lblBarrio.Text = "Barrio:";
+            // 
+            // txtbNumCalle
+            // 
+            this.txtbNumCalle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(208)))), ((int)(((byte)(159)))));
+            this.txtbNumCalle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbNumCalle.Location = new System.Drawing.Point(577, 79);
+            this.txtbNumCalle.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.txtbNumCalle.Name = "txtbNumCalle";
+            this.txtbNumCalle.Size = new System.Drawing.Size(243, 25);
+            this.txtbNumCalle.TabIndex = 36;
+            // 
+            // lblNumCalle
+            // 
+            this.lblNumCalle.AutoSize = true;
+            this.lblNumCalle.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumCalle.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumCalle.Location = new System.Drawing.Point(474, 79);
+            this.lblNumCalle.Name = "lblNumCalle";
+            this.lblNumCalle.Size = new System.Drawing.Size(94, 23);
+            this.lblNumCalle.TabIndex = 35;
+            this.lblNumCalle.Text = "Número:";
+            // 
+            // txtbCalle
+            // 
+            this.txtbCalle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(208)))), ((int)(((byte)(159)))));
+            this.txtbCalle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbCalle.Location = new System.Drawing.Point(577, 41);
+            this.txtbCalle.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.txtbCalle.Name = "txtbCalle";
+            this.txtbCalle.Size = new System.Drawing.Size(243, 25);
+            this.txtbCalle.TabIndex = 34;
+            // 
+            // lblCalle
+            // 
+            this.lblCalle.AutoSize = true;
+            this.lblCalle.BackColor = System.Drawing.Color.Transparent;
+            this.lblCalle.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalle.Location = new System.Drawing.Point(500, 43);
+            this.lblCalle.Name = "lblCalle";
+            this.lblCalle.Size = new System.Drawing.Size(68, 23);
+            this.lblCalle.TabIndex = 33;
+            this.lblCalle.Text = "Calle:";
+            // 
+            // txtbApellido
+            // 
+            this.txtbApellido.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(208)))), ((int)(((byte)(159)))));
+            this.txtbApellido.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbApellido.Location = new System.Drawing.Point(204, 77);
+            this.txtbApellido.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.txtbApellido.Name = "txtbApellido";
+            this.txtbApellido.Size = new System.Drawing.Size(243, 25);
+            this.txtbApellido.TabIndex = 32;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.BackColor = System.Drawing.Color.Transparent;
+            this.lblApellido.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(22, 79);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(98, 23);
+            this.lblApellido.TabIndex = 31;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // txtbNombre
+            // 
+            this.txtbNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(208)))), ((int)(((byte)(159)))));
+            this.txtbNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbNombre.Location = new System.Drawing.Point(204, 41);
+            this.txtbNombre.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.txtbNombre.Name = "txtbNombre";
+            this.txtbNombre.Size = new System.Drawing.Size(243, 25);
+            this.txtbNombre.TabIndex = 1;
+            // 
+            // txtbNumDoc
+            // 
+            this.txtbNumDoc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(208)))), ((int)(((byte)(159)))));
+            this.txtbNumDoc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbNumDoc.Enabled = false;
+            this.txtbNumDoc.Location = new System.Drawing.Point(204, 158);
+            this.txtbNumDoc.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.txtbNumDoc.Name = "txtbNumDoc";
+            this.txtbNumDoc.Size = new System.Drawing.Size(243, 25);
+            this.txtbNumDoc.TabIndex = 4;
+            this.txtbNumDoc.Visible = false;
+            // 
+            // txtbPasaporteNro
+            // 
+            this.txtbPasaporteNro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(208)))), ((int)(((byte)(159)))));
+            this.txtbPasaporteNro.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbPasaporteNro.Enabled = false;
+            this.txtbPasaporteNro.Location = new System.Drawing.Point(886, 116);
+            this.txtbPasaporteNro.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.txtbPasaporteNro.Name = "txtbPasaporteNro";
+            this.txtbPasaporteNro.Size = new System.Drawing.Size(131, 25);
+            this.txtbPasaporteNro.TabIndex = 5;
+            this.txtbPasaporteNro.Visible = false;
+            // 
+            // lblAvisoNroDoc
+            // 
+            this.lblAvisoNroDoc.AutoSize = true;
+            this.lblAvisoNroDoc.BackColor = System.Drawing.Color.IndianRed;
+            this.lblAvisoNroDoc.Location = new System.Drawing.Point(775, 147);
+            this.lblAvisoNroDoc.Name = "lblAvisoNroDoc";
+            this.lblAvisoNroDoc.Size = new System.Drawing.Size(0, 23);
+            this.lblAvisoNroDoc.TabIndex = 30;
+            this.lblAvisoNroDoc.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblNumDoc
+            // 
+            this.lblNumDoc.AutoSize = true;
+            this.lblNumDoc.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumDoc.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumDoc.Location = new System.Drawing.Point(22, 160);
+            this.lblNumDoc.Name = "lblNumDoc";
+            this.lblNumDoc.Size = new System.Drawing.Size(160, 23);
+            this.lblNumDoc.TabIndex = 16;
+            this.lblNumDoc.Text = "N° Documento:";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(201)))), ((int)(((byte)(79)))));
+            this.btnLimpiar.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(201)))), ((int)(((byte)(79)))));
+            this.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLimpiar.BorderColor = System.Drawing.Color.Green;
+            this.btnLimpiar.BorderRadius = 20;
+            this.btnLimpiar.BorderSize = 1;
+            this.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpiar.FlatAppearance.BorderSize = 0;
+            this.btnLimpiar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnLimpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.Color.Black;
+            this.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.btnLimpiar.Location = new System.Drawing.Point(279, 207);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(105, 49);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.TextColor = System.Drawing.Color.Black;
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            // 
+            // lblNomCli
+            // 
+            this.lblNomCli.AutoSize = true;
+            this.lblNomCli.BackColor = System.Drawing.Color.Transparent;
+            this.lblNomCli.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomCli.Location = new System.Drawing.Point(22, 43);
+            this.lblNomCli.Name = "lblNomCli";
+            this.lblNomCli.Size = new System.Drawing.Size(95, 23);
+            this.lblNomCli.TabIndex = 11;
+            this.lblNomCli.Text = "Nombre:";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(201)))), ((int)(((byte)(79)))));
+            this.btnConsultar.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(201)))), ((int)(((byte)(79)))));
+            this.btnConsultar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnConsultar.BorderColor = System.Drawing.Color.Green;
+            this.btnConsultar.BorderRadius = 20;
+            this.btnConsultar.BorderSize = 1;
+            this.btnConsultar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConsultar.FlatAppearance.BorderSize = 0;
+            this.btnConsultar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnConsultar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.btnConsultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConsultar.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.ForeColor = System.Drawing.Color.Black;
+            this.btnConsultar.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.btnConsultar.Location = new System.Drawing.Point(474, 207);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(123, 49);
+            this.btnConsultar.TabIndex = 6;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.TextColor = System.Drawing.Color.Black;
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            // 
+            // cboTipoDoc
+            // 
+            this.cboTipoDoc.FormattingEnabled = true;
+            this.cboTipoDoc.Location = new System.Drawing.Point(204, 108);
+            this.cboTipoDoc.Name = "cboTipoDoc";
+            this.cboTipoDoc.Size = new System.Drawing.Size(243, 31);
+            this.cboTipoDoc.TabIndex = 3;
+            // 
+            // lblTipoDoc
+            // 
+            this.lblTipoDoc.AutoSize = true;
+            this.lblTipoDoc.BackColor = System.Drawing.Color.Transparent;
+            this.lblTipoDoc.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoDoc.Location = new System.Drawing.Point(22, 118);
+            this.lblTipoDoc.Name = "lblTipoDoc";
+            this.lblTipoDoc.Size = new System.Drawing.Size(176, 23);
+            this.lblTipoDoc.TabIndex = 1;
+            this.lblTipoDoc.Text = "Tipo Documento:";
+            // 
+            // cboBarrio
+            // 
+            this.cboBarrio.FormattingEnabled = true;
+            this.cboBarrio.Location = new System.Drawing.Point(577, 110);
+            this.cboBarrio.Name = "cboBarrio";
+            this.cboBarrio.Size = new System.Drawing.Size(243, 31);
+            this.cboBarrio.TabIndex = 38;
             // 
             // frmClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::FarmaTown.Properties.Resources.clients;
-            this.ClientSize = new System.Drawing.Size(865, 527);
+            this.ClientSize = new System.Drawing.Size(914, 527);
+            this.Controls.Add(this.gbFiltros);
             this.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -45,10 +311,32 @@ namespace FarmaTown.Presentacion.ABMC_s.Clientes
             this.MaximizeBox = false;
             this.Name = "frmClientes";
             this.Text = "Gestión Clientes - FarmaTown";
+            this.gbFiltros.ResumeLayout(false);
+            this.gbFiltros.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbFiltros;
+        private System.Windows.Forms.Label lblBarrio;
+        private System.Windows.Forms.TextBox txtbNumCalle;
+        private System.Windows.Forms.Label lblNumCalle;
+        private System.Windows.Forms.TextBox txtbCalle;
+        private System.Windows.Forms.Label lblCalle;
+        private System.Windows.Forms.TextBox txtbApellido;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtbNombre;
+        private System.Windows.Forms.TextBox txtbNumDoc;
+        private System.Windows.Forms.TextBox txtbPasaporteNro;
+        private System.Windows.Forms.Label lblAvisoNroDoc;
+        private System.Windows.Forms.Label lblNumDoc;
+        private CustomControls.RJControls.RJButton btnLimpiar;
+        private System.Windows.Forms.Label lblNomCli;
+        private CustomControls.RJControls.RJButton btnConsultar;
+        private System.Windows.Forms.ComboBox cboTipoDoc;
+        private System.Windows.Forms.Label lblTipoDoc;
+        private System.Windows.Forms.ComboBox cboBarrio;
     }
 }
--- FarmaTown/Presentacion/ABMC's/Clientes/frmClientes.cs
+++ FarmaTown/Presentacion/ABMC's/Clientes/frmClientes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FarmaTown.Logica;
+using FarmaTown.Presentacion.Servicios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,65 @@ namespace FarmaTown.Presentacion.ABMC_s.Clientes
 {
     public partial class frmClientes : Form
     {
+        Cliente oCliente;
+        TipoDocumento oTipoDoc;
+        Barrio oBarrio;
         public frmClientes()
         {
+            oCliente = new Cliente();
+            oTipoDoc = new TipoDocumento();
+            oBarrio = new Barrio();
             InitializeComponent();
+        }
+        private void frmClientes_Load(object sender, EventArgs e)
+        {
+            ComboBoxService.cargarCombo(this.cboTipoDoc, oTipoDoc.recuperarTodos(false)
+                , "nombre", "idTipo");
+            ComboBoxService.cargarCombo(this.cboBarrio, oBarrio.recuperarTodos(false)
+                , "nombre", "idBarrio");
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            /*
+             * Verifica la correcta selección
+             * para el formulario y filtra
+             */
+            if (this.validarCampos())
+            {
+                List<Cliente> resultadosClientes = new List<Cliente>();
+                string nom = this.txtbNombre.Text;
+                string apellido = this.txtbApellido.Text;
+                int idTipoDoc;
+                string nroDoc = this.txtbNumDoc.Text;
+                string calle = this.txtbCalle.Text;
+                string numCalle = this.txtbNumCalle.Text;
+                //int idCliente;
+                //int idBarrio;
+               
+
+                if (this.cboTipoDoc.SelectedIndex == -1)
+                {
+                    idTipoDoc = -1;
+                }
+                else
+                {
+                    idTipoDoc = (int)this.cboTipoDoc.SelectedValue;
+                    if (idTipoDoc == 2)
+                    {
+
+                        string letras = this.txtbNumDoc.Text;
+                        string nros = this.txtbPasaporteNro.Text;
+                        nroDoc = letras + nros;
+                    }
+                    else
+                        nroDoc = this.txtbNumDoc.Text;
+                }
+                //falta pasarle idCliente
+                resultadosClientes = this.oCliente.recuperarConParam(nom, apellido, nroDoc, idTipoDoc, calle, numCalle);
+                //this.cargarGrilla(this.dgvClientes, resultadosClientes);
+            }
+
         }
     }
 }
